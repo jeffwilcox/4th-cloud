3be04fa30e864857da36efd9cb49bc771b981eff
Jeff Wilcox <jeffwilcox@gmail.com>
2012-05-10 14:14:39 -0700

    removing winston logger

--- debugOnePerson.js
+++ debugOnePerson.js
@@ -27,15 +27,6 @@
 // agent, so it is sending hundreds of thousands of requests to fsq, Mongo,
 // MPNS, etc.
 
-var winston = require('winston');
-
-var logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)()
-    ]
-  });
-logger.cli();
-
 var role = 'web';
 var mode = 'dev';
 
@@ -52,9 +43,9 @@ if (process.argv.length > 2) {
 // Prepare configuration. Startup once ready.
 var configuration = require('./lib/configuration')(role, mode);
 var cleanup = require('./lib/cleanup');
-var context = require('./lib/context')(configuration, role, mode, logger, function(err) {
+var context = require('./lib/context')(configuration, role, mode, function(err) {
     if(err) {
-        logger.error('Unfortunately startup went badly and the context could not be prepared.');
+        console.error('Unfortunately startup went badly and the context could not be prepared.');
         console.dir(err);
     } else {
         console.log('Cleaning up...');
@@ -66,34 +57,13 @@ var context = require('./lib/context')(configuration, role, mode, logger, functi
     }
 });
 
-var mpns = require('mpns');
 var async = require('async');
 
-var http = require('http');
-var url = require('url');
-var crypto = require('crypto');
 var path = require('path');
 var fs = require('fs');
-var querystring = require('querystring');
-var request = require('request');
 
-var dateutil = require('./lib/dateutil');
-var geo = require('./lib/geo');
 var pushutil = require('./lib/pushutil');
-var helpers = require('./lib/helpers');
-var httputil = require('./lib/httputil');
-var mayorlivetile = require('./lib/mayorlivetile');
 
-// Random wild configuration values
-// TODO: Move more of these to configuration...
-var lastQuerySize = 0;
-
-var inflightClients = 0;
-var healthMinimumInFlight = 70; // 50;
-var healthMinimumGrowth = 45; // 25; // kick off 5 when under 10.
-var healthIntervalMs = 1000; // every 5 seconds.
-
-// TODO: Refactor better.
 context.configuration.path.temporaryPhotosDirectory = context.environment.isWindows === true ? "c:\\temp\\49photos\\" : '/tmp/49photos/'
 
 function startupWorkerRole() {
