eafab7f0bf5d88cf77bda1c9090b3a688e7b9dc5
Jeff Wilcox <jeffwilcox@gmail.com>
2012-05-10 14:14:39 -0700

    removing winston logger

--- server.js
+++ server.js
@@ -27,15 +27,6 @@
 // agent, so it is sending hundreds of thousands of requests to fsq, Mongo,
 // MPNS, etc.
 
-var winston = require('winston');
-
-var logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)()
-    ]
-  });
-logger.cli();
-
 var role = 'web'; // The default.
 var mode = 'production';
 if (process.argv.length > 2) {
@@ -48,55 +39,29 @@ if (process.argv.length > 3) {
 // Prepare configuration. Startup once ready.
 var configuration = require('./lib/configuration')(role, mode);
 var cleanup = require('./lib/cleanup');
-var context = require('./lib/context')(configuration, role, mode, logger, function(err) {
+var context = require('./lib/context')(configuration, role, mode, function(err) {
     if(err) {
-        logger.error('Unfortunately startup went badly and the context could not be prepared.');
+        console.error('Unfortunately startup went badly and the context could not be prepared.');
         console.dir(err);
     } else {
-        console.log('Cleaning up...');
         cleanup.initialize(context);
-
-        // We're rocking!
-        console.log('Starting up the role...');
         startupWorkerRole();
     }
 });
 
 // TODO: Support Foursquare real-time push notifications & the system around that concept.
 
-var mpns = require('mpns');
 var async = require('async');
 
-var http = require('http');
-var url = require('url');
-var crypto = require('crypto');
 var path = require('path');
 var fs = require('fs');
-var querystring = require('querystring');
-var request = require('request');
 
-var dateutil = require('./lib/dateutil');
-var geo = require('./lib/geo');
 var pushutil = require('./lib/pushutil');
-var helpers = require('./lib/helpers');
-var httputil = require('./lib/httputil');
-var mayorlivetile = require('./lib/mayorlivetile');
 
-// Random wild configuration values
-// TODO: Move more of these to configuration...
-var lastQuerySize = 0;
-
-var inflightClients = 0;
-var healthMinimumInFlight = 70; // 50;
-var healthMinimumGrowth = 45; // 25; // kick off 5 when under 10.
-var healthIntervalMs = 1000; // every 5 seconds.
-
-// TODO: Refactor better.
 context.configuration.path.temporaryPhotosDirectory = context.environment.isWindows === true ? "c:\\temp\\49photos\\" : '/tmp/49photos/'
 
 function startupWorkerRole() {
     if (!path.existsSync(context.configuration.path.temporaryPhotosDirectory)) {
-        context.logger.silly('creating the ' + context.configuration.path.temporaryPhotosDirectory + ' local storage directory');
         fs.mkdirSync(context.configuration.path.temporaryPhotosDirectory);
     }
 
@@ -108,7 +73,7 @@ function startupWorkerRole() {
         var app = webserver.initialize(context);
         app.listen(3000);
         
-        logger.info('Web server is listening on port: 3000 ' + app.settings.env);
+        console.log('Web server is listening on port: 3000 ' + app.settings.env);
     }
 
     // ---------------------------------------------------------------------------
