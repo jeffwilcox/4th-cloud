6c60d7c49578c99350c0195927a68578bd5fe747
Jeff Wilcox <jwgithub@outlook.com>
2013-09-24 13:52:52 -0700

    bootstrapping new web load framework for many sites

---
+++ web/index.js
@@ -0,0 +1,44 @@
+//
+// Copyright (C) Jeff Wilcox
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+var express = require('express');
+
+require('../lib/configuration')('web', function (config) {
+    var appName = config.runtime.app;
+    if (appName != 'www') {
+        throw new Error('Unsupported known web service: ' + appName);
+    }
+
+    require('../lib/context').initialize(config, function (err, context) {
+        if (err) {
+            context.stats.server.startupFail();
+            context.winston.error('This web server is not a web role or could not be started', { error: err });
+            throw new Error(err);
+        } else {
+            var app = express();
+            var site = require('./' + appName + '/')(app, context);
+
+            // var app = webserver.initialize(context);
+
+            var port = context.configuration.hosting.port;
+            app.set('port', port);
+            app.listen(port);
+            
+            context.stats.server.startup();
+            context.winston.silly('Web server is listening on port: ' + port + ' ' + app.settings.env);
+        }
+    });
+});
---
+++ web/www/index.js
@@ -0,0 +1,28 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+module.exports = function (app, context) {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+
+	// development only
+	if ('development' == app.get('env')) {
+	  app.use(express.errorHandler());
+	}
+
+	app.get('/', routes.index);
+	app.get('/users', user.list);
+}
